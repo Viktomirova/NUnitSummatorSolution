294eab566d6a6b1349f7220464ef942e13c811f9
Svetlana Viktomirova <86418476+Viktomirova@users.noreply.github.com>
2022-05-06 18:50:56 +0300

    Added files

---
+++ Summator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SummatorClass.Sum(new int[] {1, 2, 3, 4, 5, 6}));
+            Console.WriteLine(SummatorClass.Average(new int[] {-5, -8}));
+        }
+    }
+}
