59dd8dde9b49e26561dc9ea3760fe4a68b2d1d49
Svetlana Viktomirova <86418476+Viktomirova@users.noreply.github.com>
2022-05-06 17:21:44 +0300

    Add files

---
+++ Summator/SummatorClass.cs
@@ -0,0 +1,25 @@
+﻿namespace Summator
+{
+    public static class SummatorClass
+    {
+        public static int Sum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+        
+        public static double Average(int[] arr)
+        {
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum/arr.Length;
+        }
+    }
+}
